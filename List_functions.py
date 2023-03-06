4711268df7a53e9d0d14633b139d6ac09edd13c9
LR <leonricardo140@gmail.com>
2023-03-06 22:27:08 +0000

    ListFunctions

---
+++ List_functions.py
@@ -0,0 +1,18 @@
+luckyNumbers = [7, 9, 21, 48, 16, 38]
+CarModels = ["BMW", "Mercedes", "Porsche", "Bentley", "Audi",]
+
+#carModels.extend(luckyNumbers) this will add lucky numbers at the end of the car model list
+
+#carModels.append("Ford") this will add the name into the list
+
+#carModels.insert(1, "Vauxhall") this will add Vauxhall to the number 1 spot in the list so after BMW
+
+#carModels.remove("BMW") This will remove BMW from the list
+
+#carModels.pop() This will pop an item off the end of the list, so Audi would be removed
+
+#carModels.index("BMW") will help find if an item is in the list, if name not in the list an error will return
+
+#carModels2 = carModels.copy() this will copy the current list to another variable.
+
+print(CarModels)
