faab9189431916b75cfb45d36cc97c277d0dc251
LR <leonricardo140@gmail.com>
2023-03-06 20:04:55 +0000

    modified:   Variables_and_Data_Types.py Changes to current file

---
+++ Variables_and_Data_Types.py
@@ -0,0 +1,8 @@
+characterName = "Leon"
+characterAge = "31"
+
+print("There once was a man name " + characterName + ", ")
+print("he was " + characterAge + " years old. ")
+print("He really liked the name " + characterName + ", ")
+print("but didn't like being " + characterAge + ".")
+
