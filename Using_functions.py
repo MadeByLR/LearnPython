73c1761ae0030c210bd9747b1558463f9b27d243
LR <leonricardo140@gmail.com>
2023-03-06 23:16:30 +0000

    File working with Functions

---
+++ Using_functions.py
@@ -0,0 +1,8 @@
+#When using a function you will need to start of with def
+#You can also add parameters within the (), another way to think of it is a variable.
+#At the def part you add the key and at the execution of the function you give it the value you want it to use.
+
+def sayhi(name, age):
+    print("Hello " + name + ", you are " + age + ".")
+#this is the command to execute function
+sayhi("Leon", "31")
