d59c8a17f1974ceef35ea0f273e7f15080f53ac5
LR <leonricardo140@gmail.com>
2023-03-06 20:13:45 +0000

    No comment added

--- Variables_and_Data_Types.py
+++ Variables_and_Data_Types.py
@@ -1,8 +1,10 @@
 characterName = "Leon"
 characterAge = "31"
+isMale = True
 
 print("There once was a man name " + characterName + ", ")
 print("he was " + characterAge + " years old. ")
+
+characterName = "Ricardo"
 print("He really liked the name " + characterName + ", ")
 print("but didn't like being " + characterAge + ".")
-
