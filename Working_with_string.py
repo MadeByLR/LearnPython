b5a2657563a558cbccff76f3779b1eeb8af79807
LR <leonricardo140@gmail.com>
2023-03-06 21:07:05 +0000

    General examples of working with Strings

---
+++ Working_with_string.py
@@ -0,0 +1,17 @@
+# #\n puts the next word on a new line shown on line 2
+# print("Hello\nWorld")
+
+# #To add " within text like "Hello"World" add \before the " shown in line 4
+# print("Hello\"World")
+
+# #Using variables and printing, also adding text on the end of it.
+#phrase = "Hello Leon"
+# print(phrase + " ,how are you?")
+
+phrase = "Hello Leon"
+#.upper() sets text to upper case, .isupper() gives True return
+print(phrase.upper().isupper())
+
+#replacing text with the .replace function. after the , is the word that will be used to replace.
+phrase = "Hello Leon"
+print(phrase.replace("Leon", "Ricardo"))
