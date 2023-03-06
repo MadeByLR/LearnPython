d6ef9f8df01ae52083fbd51b773fec9e35cddedb
LR <leonricardo140@gmail.com>
2023-03-06 21:36:51 +0000

    Creating a basic calculater

---
+++ Building_a_calculater.py
@@ -0,0 +1,5 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter a second number: ")
+result = int(num1) + int(num2)  #int(num1) this converts the string to a number
+                                #this adds the numbers rather than just putting the number together.
+print(result)
