a563abe95e9e225f2a2357b181bf086edc1c2609
LR <leonricardo140@gmail.com>
2023-03-08 19:28:54 +0000

    modified:   Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -8,4 +8,29 @@ def getChoices():
     return choices
 
 def checkWin(player, computer):
-    return[player, computer]
+    print(f"You chose: {player}, computer chose: {computer}")
+    if player == computer:
+        return "It's a tie"
+    
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose!"
+    
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose!"
+    
+    elif player == "Scissors":
+        if computer == "Paper":
+            return "Scissors cuts Paper! You win!"
+        else:
+            return "Rock smashes Scissors! You lose!"
+
+choices = getChoices()    
+result = checkWin(choices["player"], choices ["computer"])
+
+print(result)
