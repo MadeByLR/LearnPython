d806afedbb594289bedf9c0868638ad01c95faf1
LR <leonricardo140@gmail.com>
2023-03-08 21:17:03 +0000

    Made changes to couple lines for invalid comment

---
+++ Built_calculator_without_tutorial.py
@@ -0,0 +1,19 @@
+#building a calculator
+
+firstNumber = int(input("Enter first number: "))
+op = input("Enter operator: ")
+secondNumber = int(input("Enter second number: "))
+validOperators = ["+", "-", "*", "/"]
+
+if op in validOperators:
+    if op == "+":
+        print(firstNumber + secondNumber)
+    elif op == "-":
+            print(firstNumber - secondNumber)
+    elif op == "*":
+        print(firstNumber * secondNumber)
+    elif op == "/":
+        print(firstNumber / secondNumber)
+else:
+     print("Invalid Operator Used!")
+
